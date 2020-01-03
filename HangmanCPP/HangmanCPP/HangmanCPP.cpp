b5d4d5dfefb8f2115b69ecf0ab99b8362b693715
Steven-Kirby <stevenkirbywork@gmail.com>
2020-01-03 00:05:38 +0000

    Added main functionality

--- HangmanCPP/HangmanCPP/HangmanCPP.cpp
+++ HangmanCPP/HangmanCPP/HangmanCPP.cpp
@@ -3,17 +3,50 @@
 #include <time.h>
 
 using namespace std;
-string words[6] = { "pig", "cat", "dog", "fish", "horse", "parrot"};
+string words[9] = { "pig", "cat", "dog", "fish", "horse", "parrot", "laptop", "water", "football"};
+
 
 string GenerateWord();
 vector<char> SeperateWord(string);
+vector<char> HideWord(vector<char>);
+void UserGuess(char);
 
+vector<char> hiddenWord;
+vector<char> seperatedWord;
+string generatedWord;
+
+bool gamewon = false;
+int lives = 5;
 
 int main()
 {
-    cout << "Welcome to Hangman, written in C++\n";
+
+    cout << "Welcome to Hangman, written in C++" << endl; 
     srand(time(NULL));
-    SeperateWord(GenerateWord());
+    generatedWord = GenerateWord();
+    seperatedWord = SeperateWord(generatedWord);
+
+    cout << "Guess this word!" << endl;
+    hiddenWord = HideWord(seperatedWord);
+
+    char userguess;
+
+    while (!gamewon) {
+        cin >> userguess;
+        
+        UserGuess(userguess);
+        
+        for (char c : hiddenWord) {
+            cout << c;
+            cout << " ";
+        }
+
+        cout << endl << endl;
+    }
+
+    cout << "You Won!" << endl;
+   
+   
 }
 
 string GenerateWord() {
@@ -25,9 +58,6 @@ string GenerateWord() {
         
     generatedWord = wordList[randomWordIndex];
 
-    cout << "Generated word : " + generatedWord << endl;
-
-
     return generatedWord;
 }
 
@@ -36,8 +66,47 @@ vector<char> SeperateWord(string generatedWord) {
     
     for (char c : generatedWord) {
         seperatedWord.push_back(c);
-        cout << c << endl;
     }
-    
+        
     return seperatedWord;
+}
+
+vector<char> HideWord(vector<char> seperatedWord) {
+    vector<char> hiddenWord;
+    for (char c : seperatedWord) {
+        hiddenWord.push_back('_');
+        cout << "_ ";
+    }
+    cout << endl;
+    return hiddenWord;
+}
+
+void UserGuess(char userGuess)
+{
+
+    bool guessSuccessful = false;
+    gamewon = true;
+    for (int i = 0; i < seperatedWord.size(); i++) {
+        if (seperatedWord[i] == userGuess) {
+            hiddenWord[i] = userGuess;
+            guessSuccessful = true;
+        }
+
+        if (hiddenWord[i] == '_') {
+            gamewon = false;
+        }
+    }
+
+    if (guessSuccessful == false) {
+        printf("\nLives Left %d\n", --lives);
+        if (lives < 1) {
+            cout << "You Lose!" << endl;
+            cout << "The word was.... " << generatedWord << endl;
+            exit(1);
+            
+        }
+    }
+        
+
+
 }
